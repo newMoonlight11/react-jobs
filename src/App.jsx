9925e7525424c5294f40d0febdea8480bb97b420
newMoonlight11 <104524751+newMoonlight11@users.noreply.github.com>
2025-07-14 22:16:45 -0500

    first simple examples

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const App = () => {
+  const name = 'Cami';
+  const x = 10;
+  const y = 20;
+  const names = ['Brad', 'Mary', 'Joe', 'Sara'];
+  const loggedIn = true;
+
+  return (
+    <>
+    <div className='text-5xl'>
+      App
+    </div>
+    <p>Hello {name}</p>
+    <p>The sum of {x} and {y} is {x+y}</p>
+    <ul>
+      {names.map((name, index)=>(
+        <li key={index}>{name}</li>
+      ))}
+    </ul>
+    {loggedIn && <h1>Hello Member</h1>}
+    </>
+  )
+}
+
+export default App
+
